db5a5477e4a0e8700c2415b26b3bdc21b4c4f2bb
Yarrik Vanwalleghem <yarrikvanwalleghem@gmail.com>
2022-04-16 18:41:28 +0200

    added fions w/o testing

--- W7/quaternions.py
+++ W7/quaternions.py
@@ -1,5 +1,9 @@
-class Quaternion():
-    """ 
+def sign(n):
+    return "+" if n >= 0 else "-"
+
+
+class Quaternion:
+    """
     >>> q1 = Quaternion(2, 4, 7, 3)
     >>> q1.norm()
     8.831760866327848
@@ -31,11 +35,49 @@ class Quaternion():
     >>> q1 * 3
     Quaternion(6, 12, 21, 9)
     >>> 3 * q1
-    Quaternion(6, 12, 21, 9) 
+    Quaternion(6, 12, 21, 9)
     """
 
-    def __init__(self, a, b, c, d) -> None:
-        self.z = a
-        self.i = b
-        self.j = c
-        self.k = d
+    def __init__(self, a=0, b=0, c=0, d=0) -> None:
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def __str__(self) -> str:
+        return (
+            str(self.a)
+            + sign(self.b)
+            + str(abs(self.b))
+            + sign(self.c)
+            + str(abs(self.c))
+            + sign(self.d)
+            + str(abs(self.d))
+        )
+
+    def __repr__(self) -> str:
+        return (
+            "Quaternion(" + self.a + ", " + self.b + ", " + self.c + ", " + self.d + ")"
+        )
+
+    def norm(self):
+        """
+        De methode moet de norm (float) teruggeven.
+        """
+        return sqrt(
+            self.a * self.a + self.b * self.b + self.c * self.c + self.d * self.d
+        )
+
+    def __add__(self, other):
+        a = self.a + other.a
+        b = self.b + other.b
+        c = self.c + other.c
+        d = self.d + other.d
+        return Quaternion(a, b, c, d)
+
+    def __mul__(self, other):
+        a = self.a * other.a - self.b * other.b - self.c * other.c - self.d * other.d
+        b = self.a * other.b + self.b * other.a + self.c * other.d - self.d * other.c
+        c = self.a * other.c - self.b * other.d + self.c * other.a + self.d * other.b
+        d = self.a * other.d + self.b * other.c - self.c * other.b + self.d * other.a
+        return Quaternion(a, b, c, d)
